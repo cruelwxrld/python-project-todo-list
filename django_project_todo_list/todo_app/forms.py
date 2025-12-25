58d378ff5c534eeef62e64d947e7789d0b4609b0
skywh <skywhymuted@gmail.com>
2025-12-25 20:36:03 +0800

    final

--- django_project_todo_list/todo_app/forms.py
+++ django_project_todo_list/todo_app/forms.py
@@ -1,13 +1,7 @@
 from django import forms
 from .models import Task
 
-class TaskForm(forms.Form):
+class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'completed']
-        widgets = {}
-        labels = {
-            'title': 'Task Title',
-            'description': 'Task Description',
-            'completed': 'Task Completed',
-        }
